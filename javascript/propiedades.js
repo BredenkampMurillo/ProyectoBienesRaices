5a703264fbf3ab47a6cd3ad6edf4b21722577983
BredenkampMurillo <oscarbredenkmap@gmail.com>
2025-07-10 21:31:46 -0600

    Update propiedades.js

--- javascript/propiedades.js
+++ javascript/propiedades.js
@@ -1,7 +1,7 @@
 //PARTE DE LAS PROPIEDADES
 
 $(document).ready(function () {
-    const apiURL = "http://127.0.0.1:5500/propiedades.json";
+    const apiURL = "./propiedades.json";
     function fetchProperties() {
       $.ajax({
         url: apiURL,
